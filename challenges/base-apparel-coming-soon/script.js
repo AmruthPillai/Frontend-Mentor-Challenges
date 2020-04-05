0c2850de93b92e23341967578862b81cd62b3866
Amruth Pillai <im.amruth@gmail.com>
2020-04-05 23:19:49 +0530

    completed base apparel coming soon challenge

---
+++ challenges/base-apparel-coming-soon/script.js
@@ -0,0 +1,18 @@
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+function onSubmit() {
+  var emailInput = document.getElementById("emailInput");
+  var errorText = document.getElementById("errorText");
+  var emailAddress = document.getElementById("emailAddress").value;
+
+  if (validateEmail(emailAddress)) {
+    emailInput.classList.remove("incorrect");
+    errorText.classList.add("invisible");
+  } else {
+    emailInput.classList.add("incorrect");
+    errorText.classList.remove("invisible");
+  }
+}
