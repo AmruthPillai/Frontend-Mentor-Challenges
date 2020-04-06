e253e1e582972dad76ed42dedc9e6e09c4dd7a3e
Amruth Pillai <im.amruth@gmail.com>
2020-04-06 15:29:10 +0530

    added solution for ping-coming-soon-page

--- README.md
+++ README.md
@@ -4,7 +4,5 @@ A directory of solutions to the challenges put up by
 <a href="https://frontendmentor.io">frontendmentor.io</a>.  
 All of the solutions use plain ol&apos; HTML, CSS &amp; Javascript.
 
----
-
 **View Solutions Here:**  
 **https://amruthpillai.github.io/Frontend-Mentor-Challenges/**
---
+++ challenges/ping-coming-soon-page/script.js
@@ -0,0 +1,15 @@
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+function onSubmit() {
+  var formElem = document.getElementsByTagName("form")[0];
+  var emailAddress = document.getElementById("emailAddress").value;
+
+  if (validateEmail(emailAddress)) {
+    formElem.classList.remove("incorrect");
+  } else {
+    formElem.classList.add("incorrect");
+  }
+}
